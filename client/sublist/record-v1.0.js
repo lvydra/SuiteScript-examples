b310bc749c9be4ebf187dee50d66a221456f3724
lvydra <torgal@hotmail.cz>
2018-04-06 10:19:20 +0200

    Sublists on client

--- client/sublist/record-v1.0.js
+++ client/sublist/record-v1.0.js
@@ -1,24 +1,28 @@
 function pageInit(type) {
-	var prodPrefCount = nlapiGetLineItemCount('');
+	alert('INIT');
+
+	var prodPrefCount = nlapiGetLineItemCount('recmachcustrecord_sdr_prod_pref_customer');
 
 	alert('This customer has ' + prodPrefCount + ' product preferences');
 }
 
 function lineInit(type) {
-	if (type == '') {
-		var stPrefQty = nlapiGetCurrentLineItemValue(type, '');
+	if (type == 'recmachcustrecord_sdr_prod_pref_customer') {
+		var stPrefQty = nlapiGetCurrentLineItemValue(type,
+				'custrecord_sdr_prod_pref_qty');
 
 		if (stPrefQty == '') {
-			nlapiSetCurrentLineItemValue('', 1);
+			nlapiSetCurrentLineItemValue('custrecord_sdr_prod_pref_qty', 1);
 		}
 	}
 }
 
 function validateLine(type) {
-	if (type == '') {
-		var stPrefQty = nlapiGetCurrentLineItemValue(type, '');
+	if (type == 'recmachcustrecord_sdr_prod_pref_customer') {
+		var stPrefQty = nlapiGetCurrentLineItemValue(type,
+				'custrecord_sdr_prod_pref_qty');
 
-		if (parseInt(stPrefQty) == '') {
+		if (parseInt(stPrefQty) > 10) {
 			alert('You have selected a preferred quantity that exeeds limit of 10');
 
 			return false;
