baf0e6bee492a519c74c58598ef0b8c958e3f643
lvydra <torgal@hotmail.cz>
2018-04-13 13:45:37 +0200

    Record creating

--- client/record/rc-event-handling.js
+++ client/record/rc-event-handling.js
@@ -15,3 +15,36 @@ function saveRecord() {
 		return false;
 	}
 }
+
+function createSalesOrder() {
+	alert('start createSalesOrder');
+
+	var reSalesOrder = nlapiCreateRecord('salesorder');
+
+	var customerId = nlapiGetRecordId();
+
+	reSalesOrder.setFieldValue('entity', customerId);
+
+	reSalesOrder.selectNewLineItem('item');
+	reSalesOrder.setCurrentLineItemValue('item', 'item', '36');
+	reSalesOrder.setCurrentLineItemValue('item', 'amount', '7');
+	reSalesOrder.commitLineItem('item');
+
+	reSalesOrder.selectNewLineItem('item');
+	reSalesOrder.setCurrentLineItemValue('item', 'item', '37');
+	reSalesOrder.setCurrentLineItemValue('item', 'amount', '2');
+	reSalesOrder.commitLineItem('item');
+
+	alert('submit record createSalesOrder');
+
+	var salesOrderId = nlapiSubmitRecord(reSalesOrder);
+
+	alert('create url createSalesOrder');
+
+	var salesOrderURL = nlapiResolveURL('RECORD', 'salesorder', salesOrderId,
+			false);
+
+	alert('open window createSalesOrder');
+
+	window.open(salesOrderURL, '_blank');
+}
--- server/user-event/ue-customer.js
+++ server/user-event/ue-customer.js
@@ -37,4 +37,49 @@ function afterSubmit() {
 				'Welcome. We are glad for you to be here.', null, null,
 				arrRecordsForAttach);
 	}
+
+	var arrInitializations = new Array();
+	arrInitializations['customfrom'] = '-120';
+
+	var recTask = nlapiCreateRecord('task', arrInitializations);
+
+	var salesRepId = recCustomer.getFieldValue('salesRepId');
+	var stCCEmail = '';
+
+	if (salesRepId != '') {
+		recTask.setFieldValue('assigned', salesRepId);
+
+		var recEmployee = nlapiLoadRecord('employee', salesRepId);
+		var stSalesRepEmail = recEmployee.getFieldValue('email');
+
+		if (stSalesRepEmail != '') {
+			stCCEmail = stSalesRepEmail;
+		}
+	} else {
+		stCCEmail = null;
+	}
+
+	var stEmail = recCustomer.getFieldValue('email');
+
+	if (stEmail != '') {
+		nlapiSendEmail(
+				userId,
+				stEmail,
+				'Welcome to SuiteDreams',
+				'Welcome. We are glad for you to be a customer of SuiteDreams.',
+				stCCEmail, null, arrRecordsForAttach);
+	}
+
+	recTask.setFieldValue('message', 'Please take care of this customer.');
+	recTask.setFieldValue('company', customerId);
+	recTask.setFieldText('priority', 'High');
+
+	var stStartDate = recTask.getFieldValue('startdate');
+	var dtStartDate = nlapiStringToDate(stStartDate);
+	var dtDueDate = mlapiAddDays(dtStartDate, 30);
+	var stDueDate = nlapiDateToString(dtDueDate);
+
+	redTask.setFieldValue('duedate', stDueDate);
+
+	nlapiSubmitRecord(recTask);
 }
